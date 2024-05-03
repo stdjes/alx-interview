380bba835cfd6d6d3e084c2e684e0123db52e5b3
stdjes <133016938+stdjes@users.noreply.github.com>
2024-05-03 22:48:20 +0100

    update checker errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,62 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
+"""Implement Log parsing Algorithm"""
 
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
+import re
+import sys
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
+def print_stats(status_codes, total_size):
+    """Print All statistics"""
+    print(f'File size: {total_size}')
+    for key in sorted(status_codes.keys()):
+        if status_codes[key] == 0:
+            continue
+        print(f'{key}: {status_codes[key]}')
 
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
 
-if __name__ == "__main__":
-    import sys
+def main():
+    """Entry point of implementation"""
+    pattern = (r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+               r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "GET '
+               r'\/projects\/260 HTTP\/1.1" (\d{3}) (\d+)$')
+    line_count = 0
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    total_size = 0
 
     try:
         for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
+            line_count += 1
+            line_match = re.match(pattern, line)
+            if not line_match:
+                continue
 
-            line = line.split()
+            status_code = int(line_match.group(3))
+            file_size = int(line_match.group(4))
 
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
 
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
+            total_size += file_size
 
-        print_stats(size, status_codes)
+            if line_count % 10 == 0:
+                print_stats(status_codes, total_size)
+
+        print_stats(status_codes, total_size)
 
     except KeyboardInterrupt:
-        print_stats(size, status_codes)
+        print_stats(status_codes, total_size)
         raise
+
+
+if __name__ == '__main__':
+    main()
